a5ed90fb90e3c08b9469b883da0d07a808d8cf1e
Batdelger <obatdelger@gmail.com>
2021-02-27 16:58:38 -0600

    javafx maven empty project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp to javafx.fxml;
+    exports miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp;
+}
