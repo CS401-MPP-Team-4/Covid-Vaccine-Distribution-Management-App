02e8f07f0c20b2fc98fa50054d2ee719222bc311
Batdelger <obatdelger@gmail.com>
2021-03-01 17:41:33 -0600

    Added: abstract RepositoryService class

---
+++ src/main/java/miu/compro/cs401/team4/Covid19VaccineDistributionManagementApp/DataAccess/RepositoryService.java
@@ -0,0 +1,16 @@
+package miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.DataAccess;
+
+import java.util.List;
+
+public abstract class RepositoryService<T> {
+
+	abstract public List<T> getAll();
+	
+	abstract public T getById(Integer id);
+	
+	abstract public Integer add(T model); // should return new Id
+	
+	abstract public void update(T model);
+	
+	abstract public void delete(Integer Id);
+}
