02d783a4f328b1efdb0e85d55ceaffeb702b3d2e
beeAk <se.blen.aklilu@gmail.com>
2021-03-01 21:59:18 -0600

    update login controller

--- src/main/java/miu/compro/cs401/team4/Covid19VaccineDistributionManagementApp/controllers/LoginController.java
+++ src/main/java/miu/compro/cs401/team4/Covid19VaccineDistributionManagementApp/controllers/LoginController.java
@@ -1,13 +1,34 @@
 package miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.App;
 
 import java.io.IOException;
 
 public class LoginController {
     @FXML
-    private void goToHome() throws IOException {
+    public void goToHome() throws IOException {
         App.setRoot("master");
+    }
+
+    @FXML
+    public Button login;
+
+    @FXML
+    public TextField username;
+
+    @FXML
+    public TextField password;
+
+    public void login(ActionEvent event) throws  IOException{
+        authorize();
+    }
+
+    private void authorize() {
+
+
     }
 }
