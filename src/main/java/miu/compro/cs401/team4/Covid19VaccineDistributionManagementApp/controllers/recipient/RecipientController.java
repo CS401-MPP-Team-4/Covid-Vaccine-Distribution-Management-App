2247432b1083283f4e8709d5ecb47cf5860cd9d5
JaCkIsO <jackisodev@gmail.com>
2021-03-04 06:56:31 -0600

    Add Recipient View

---
+++ src/main/java/miu/compro/cs401/team4/Covid19VaccineDistributionManagementApp/controllers/recipient/RecipientController.java
@@ -0,0 +1,4 @@
+package miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.controllers.recipient;
+
+public class RecipientController {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,7 +13,8 @@ module miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp {
     opens miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.controllers.supplier to javafx.fxml;
     opens miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.controllers.vaccinationsite to javafx.fxml;
     opens miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.controllers.candidate to javafx.fxml;
-    
+    opens miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.controllers.recipient to javafx.fxml;
+
     exports miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp;
     exports miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.controllers;
 }
