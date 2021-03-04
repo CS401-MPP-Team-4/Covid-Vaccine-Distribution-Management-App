94230574319e121ed16cfe4e4091916290c0e39c
Batdelger <obatdelger@gmail.com>
2021-03-03 18:53:31 -0600

    List and Form views can be copied.

--- src/main/java/miu/compro/cs401/team4/Covid19VaccineDistributionManagementApp/controllers/supplier/SupplierFormController.java
+++ src/main/java/miu/compro/cs401/team4/Covid19VaccineDistributionManagementApp/controllers/supplier/SupplierFormController.java
@@ -3,14 +3,29 @@ package miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.controlle
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.DataAccess.RepositoryService;
 import miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.DataAccess.SupplierService;
 import miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.crudhelper.AbstractCRUDFormController;
 import miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.models.Supplier;
+import miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.utils.Bind;
 
 public class SupplierFormController extends AbstractCRUDFormController<Supplier> {
 
 	static final RepositoryService<Supplier> repositoryService = new SupplierService();
+	
+	@FXML
+	@Bind("name")
+	TextField txtName;
+	
+	@FXML
+	@Bind("address")
+	TextField txtAddress;
+	
+	@FXML
+	@Bind("phoneNumber")
+	TextField txtPhoneNumber;
 	
 	@Override
 	public void init(URL location, ResourceBundle resources) {
