acb01c4f71417b422978f24997e2519b2f2d229b
Batdelger <obatdelger@gmail.com>
2021-03-04 02:48:41 -0600

    Some documentation added on AbstractCRUDController classes

--- src/main/java/miu/compro/cs401/team4/Covid19VaccineDistributionManagementApp/crudhelper/AbstractCRUDController.java
+++ src/main/java/miu/compro/cs401/team4/Covid19VaccineDistributionManagementApp/crudhelper/AbstractCRUDController.java
@@ -5,15 +5,32 @@ import miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.DataAccess
 import miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.models.Model;
 import miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.utils.TaskWithException;
 
+/**
+ * abstract class of the all controllers which are implementing CRUD operations
+ * */
 public abstract class AbstractCRUDController<T extends Model> {
 	public abstract RepositoryService<T> getRepositoryService();
 	
+	/**
+	 * Inherited classes must implement that method. It used to change title of app or dialog window.
+	 * */
 	public abstract String getTitle();
 	
+	/**
+	 * Runs task safely when error occurs show the error message
+	 * 
+	 * @param task {@code TaskWithException} task to run
+	 * */
 	public void runTask(TaskWithException task) {
 		runTask(task, false);
 	}
 
+	/**
+	 * Runs task safely when error occurs show the error message
+	 * 
+	 * @param task {@code TaskWithException} task to run
+	 * @param showSuccess {boolean} once task if true finished shows success message
+	 * */
 	public void runTask(TaskWithException task, boolean showSuccess) {
 		try {
 			task.run();
