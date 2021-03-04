7fa379e825564ffcede54fafe608dd4d82a0ef8e
beeAk <se.blen.aklilu@gmail.com>
2021-03-03 20:44:20 -0600

    add vaccination site view

---
+++ src/main/java/miu/compro/cs401/team4/Covid19VaccineDistributionManagementApp/controllers/VaccinationSiteController.java
@@ -0,0 +1,8 @@
+package miu.compro.cs401.team4.Covid19VaccineDistributionManagementApp.controllers;
+
+import javafx.event.ActionEvent;
+
+public class VaccinationSiteController {
+    public void registerVaccinationSite(ActionEvent event) {
+    }
+}
